4c7f13d1bb93ccf25145a33f55c1c722d0d37344
Prabhakar Mandal <prabhask856@gmail.com>
2023-06-29 11:27:37 +0530

    added form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,32 +1,40 @@
-import React from 'react';
-import './AvailableMeals.css';
+import React from "react";
+import "./AvailableMeals.css";
 
 const meals = [
-    {mealTitle: "Sushi", mealDesc: "abc", mealPrice: "$123"},
-    {mealTitle: "Sushi", mealDesc: "abc", mealPrice: "$123"},
-    {mealTitle: "Sushi", mealDesc: "abc", mealPrice: "$123"},
-    {mealTitle: "Sushi", mealDesc: "abc", mealPrice: "$123"},
-]
+  { mealTitle: "Sushi", mealDesc: "A german specialty", mealPrice: "$123" },
+  { mealTitle: "Buger", mealDesc: "A german specialty", mealPrice: "$123" },
+  { mealTitle: "Buger", mealDesc: "A german specialty", mealPrice: "$123" },
+  { mealTitle: "Buger", mealDesc: "A german specialty", mealPrice: "$123" },
+  { mealTitle: "Sushi", mealDesc: "A german specialty", mealPrice: "$123" },
+  { mealTitle: "Sushi", mealDesc: "A german specialty", mealPrice: "$123" },
+];
 
 const AvailableMeals = () => {
   return (
-    <div className='meals-list'>
-        <div className='meal'>
-            {
-                meals.map((meal, index) => {
-                    return (
-                        <div>
-                            <h3>{meal.mealTitle}</h3>
-                            <p>{meal.mealDesc}</p>
-                            <h4>{meal.mealPrice}</h4>
-                        </div>
-                    )
-                })
-            }
-        </div>
-       
+    <div className="meals-list">
+      {meals.map((meal, index) => {
+        return (
+          <div className="meal">
+            <div className="meal-card">
+              <div>
+                <h3>{meal.mealTitle}</h3>
+                <p>{meal.mealDesc}</p>
+                <h4>{meal.mealPrice}</h4>
+              </div>
+              <div className="meal-form">
+                <label>Ammount</label>
+                <input/>
+                <div >
+                <button className="btn">+Add</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default AvailableMeals
+export default AvailableMeals;
