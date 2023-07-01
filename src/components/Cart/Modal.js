e0850e0730119ddb8b33c69bfc488b060234e5ee
Prabhakar Mandal <prabhask856@gmail.com>
2023-07-01 11:15:01 +0530

    Showing and Enabling the delete of Cart Items

--- src/components/Cart/Modal.js
+++ src/components/Cart/Modal.js
@@ -1,42 +1,67 @@
-import React, { useEffect, useState } from "react";
-import "./Cart.css";
+import React, { useEffect, useState, useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+import "./Modal.css";
+import CartContext from "../stroe/cart-context";
+
+const MyModal = ({ openModal }) => {
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  const cartCtx = useContext(CartContext);
 
 
-const MyModal = ({openModal}) => {
+  useEffect(() => {
+    //stop background scrolling
+    document.body.style.overflowY = "hidden";
 
-    useEffect(() => {
-      //stop background scrolling
-      document.body.style.overflowY = "hidden";
+  cartCtx._currentValue.items.forEach((item,index) => {
+    setTotalAmount(totalAmount + Number(item.mealPrice))
+  })
+    //background scrolling / cleaner function
+    return () => {
+      document.body.style.overflowY = "scroll";
+    };
+  }, []);
 
-      //background scrolling
-      return () => {
-        document.body.style.overflowY = "scroll";
-      };
-    }, []);
-
-    return (
-      <>
-        <div className="modal-wrapper">
-          <div className="modal">
-            <div className="modal-title">
-              <span>Sushi</span>
-              <h3>Total Amount</h3>
+  return (
+    <>
+      <div className="modal-wrapper">
+        <div className="modal">
+          {cartCtx._currentValue.items.map((item, index) => {
+             return (
+              <div className="meals" key={item.id}>
+              <span style={{fontWeight:'bolder'}}>{item.mealTitle}</span>
+              <div className="meal-price">
+                <span>{item.mealPrice}</span>
+                <text className="meal-quantity">{`x${1}`}</text>
+                <button className="remove-btn"><FontAwesomeIcon icon={faMinus} /></button>
+                <button className="add-btn"><FontAwesomeIcon icon={faPlus} /></button>
+              </div>
             </div>
-            <div className="cart-value">
+             )
+          })
+          }
+          <div className="cart-value">
+            <div className="total-amount">
               <div>
-                <h3>{35.5}</h3>
+                <h3>Total Amount</h3>
               </div>
-              <div className="modal-btn">
-                <button className="btn-close" onClick={() => openModal(false)}>
-                  Close
-                </button>
-                <button className="btn-order">Order</button>
+              <div>
+                <h3>{totalAmount}</h3>
               </div>
+            </div>
+            <div className="modal-btn">
+              <button className="btn-close" onClick={() => openModal(false)}>
+                Close
+              </button>
+              <button className="btn-order">Order</button>
             </div>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
 
-  export default MyModal;
+export default MyModal;
